f3ada52fea9de644e67abe175208afb5a1dab0fe
Jina2ee <jina2ee1011@gmail.com>
2024-04-02 14:22:14 +0100

    Link between calendar and [id].tsx

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,12 +1,19 @@
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid" // a plugin!
+import { useNavigate } from "react-router-dom"
 
-export default function Calendar() {
+export default function Calendar({ films }: any) {
+  const navigate = useNavigate()
+  const eventClick = (id: string) => {
+    navigate("/diary/" + id)
+  }
   return (
     <FullCalendar
       height={"auto"}
       plugins={[dayGridPlugin]}
       initialView='dayGridMonth'
+      events={films}
+      eventClick={(e) => eventClick(e.event.id)}
     />
   )
 }
